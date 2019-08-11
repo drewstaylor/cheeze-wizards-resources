6416d3d635b3e84e6ac1374f123df3cf96dfa4da
Drew Taylor <drew@infiniteinternet.ca>
2019-08-11 00:56:55 -0400

    group wizards by the special affinity

--- wizards.js
+++ wizards.js
@@ -1,6 +1,22 @@
 const json = require('./allWizards.json');
 const Wizards = json.wizards;
 
+/***
+ * Affinities Mapping:
+ * 0 = NOTSET, 
+ * 1 = NEUTRAL, 
+ * 2 = FIRE, 
+ * 3 = WIND, 
+ * 4 = WATER
+ */
+const affinities = [
+    'Unknown',
+    'Neutral',
+    'Fire',
+    'Wind',
+    'Water'
+];
+
 const sortByPowerLevel = function (a, b) {
     let powerLevelA = Number(a.power);
     let powerLevelB = Number(b.power);
@@ -26,10 +42,29 @@ const sortByWizardId = function (a, b) {
     }
     return comparison;
 };
-// Sort Wizards By Power Level
+
+const groupWizardsByAffinity = function (a, b) {
+    let wizardAffinityA = Number(a.affinity);
+    let wizardAffinityB = Number(b.affinity);
+    let comparison = 0;
+    // Compare
+    if (wizardAffinityA > wizardAffinityB) {
+        comparison = 1;
+    } else if (wizardAffinityA < wizardAffinityB) {
+        comparison = -1;
+    }
+    a.specialPower = affinities[a.affinity];
+    return comparison;
+};
+
+// Sort Wizards by Power Level
 //let wizardsByPowerLevel = Wizards.sort(sortByPowerLevel);
 //console.log(wizardsByPowerLevel);
 
 // Sort Wizards by ID
-let wizardsById = Wizards.sort(sortByWizardId);
-console.log(wizardsById);
+//let wizardsById = Wizards.sort(sortByWizardId);
+//console.log(wizardsById);
+
+// Group Wizards by Affinty Type
+let wizardsByAffinity = Wizards.sort(groupWizardsByAffinity);
+console.log(wizardsByAffinity);
