8cde931c20ba4fbacb30824458eab6ad909e2547
Drew Taylor <drew@infiniteinternet.ca>
2019-08-11 02:16:28 -0400

    sort wizards by growth rate and reverse

--- poc/wizards.js
+++ poc/wizards.js
@@ -90,6 +90,33 @@ const sortByPowerLevel = function (a, b) {
 };
 
 /**
+ * Sort the Wizards array by Wizard power level growth
+ * e.g. Wizards that have accummulated the highest power gains througout the tournament duration
+ * @param {Number} a 
+ * @param {Number} b 
+ */
+const sortByPowerLevelGrowth = function (a, b) {
+    // Initialization params
+    let initialPowerLevelA = Number(a.initialPower);
+    let powerLevelA = Number(a.power);
+    let initialPowerLevelB = Number(b.initialPower);
+    let powerLevelB = Number(b.power);
+    // Get growth rates
+    let growthMarginA = powerLevelA - initialPowerLevelA;
+    let growthMarginB = powerLevelB - initialPowerLevelB;
+    // Set growth rate on sorted el.
+    a.powerGrowth = growthMarginA;
+    // Compare
+    let comparison = 0;
+    if (growthMarginA > growthMarginB) {
+        comparison = 1;
+    } else if (growthMarginA < growthMarginB) {
+        comparison = -1;
+    }
+    return comparison;
+};
+
+/**
  * Sort the Wizards array by Wizard unique ID
  * @param {Number} a 
  * @param {Number} b 
@@ -149,15 +176,27 @@ const groupWizardsByAffinity = function (a, b) {
     return comparison;
 };
 
+/**
+ * ROUTINES - Uncomment any routing and debug log to run
+ */
+
 // Sort Wizards by Power Level
-let wizardsByPowerLevel = Wizards.sort(sortByPowerLevel);
+//let wizardsByPowerLevel = Wizards.sort(sortByPowerLevel);
 //console.log(wizardsByPowerLevel);
 
 // Sort Wizards by ID
-let wizardsById = Wizards.sort(sortByWizardId);
+//let wizardsById = Wizards.sort(sortByWizardId);
 //console.log(wizardsById);
 
 // Group Wizards by Affinty Type
-let wizardsByAffinity = Wizards.sort(groupWizardsByAffinity);
+//let wizardsByAffinity = Wizards.sort(groupWizardsByAffinity);
 //console.log(wizardsByAffinity);
 //console.log(wizardsByAffinity.reverse());
+
+// Sort Wizards by biggest power gains (ascending power levels)
+//let wizardsByGrowth = Wizards.sort(sortByPowerLevelGrowth);
+//console.log(wizardsByGrowth)
+
+// Sort Wizards by weakest power gains (diminishing power levels)
+//let wizardsByDiminishingGrowth = wizardsByGrowth.reverse();
+//console.log(wizardsByDiminishingGrowth);
