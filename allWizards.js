4a91f2b38359d329cfe422b94a667d82a8e9b939
Drew Taylor <drew@infiniteinternet.ca>
2019-08-11 00:09:24 -0400

    sort wizards by power level

---
+++ allWizards.js
@@ -0,0 +1,19 @@
+const json = require('./allWizards.json');
+
+let wizards = json.wizards;
+
+const sortByPowerLevel = function (a, b) {
+    let powerLevelA = Number(a.power);
+    let powerLevelB = Number(b.power);
+    let comparison = 0;
+    // Compare
+    if (powerLevelA > powerLevelB) {
+        comparison = 1;
+    } else if (powerLevelA < powerLevelB) {
+        comparison = -1;
+    }
+    return comparison;
+};
+
+wizards.sort(sortByPowerLevel);
+console.log(wizards);
